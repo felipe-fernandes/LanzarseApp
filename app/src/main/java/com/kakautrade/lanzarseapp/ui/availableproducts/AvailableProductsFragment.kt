fa304accf4ddfb6ec29a74133abf5e33c98dd6de
felipe-fernandes <felipe.a.fernandes@gmail.com>
2024-12-30 15:51:54 -0300

    refactor AvailableProductsFragment.kt
    Signed-off-by: felipe-fernandes <felipe.a.fernandes@gmail.com>

--- app/src/main/java/com/kakautrade/lanzarseapp/ui/availableproducts/AvailableProductsFragment.kt
+++ app/src/main/java/com/kakautrade/lanzarseapp/ui/availableproducts/AvailableProductsFragment.kt
@@ -1,64 +1,42 @@
 package com.kakautrade.lanzarseapp.ui.availableproducts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.kakautrade.lanzarseapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kakautrade.lanzarseapp.databinding.FragmentAvailableProductsListBinding
 import com.kakautrade.lanzarseapp.ui.availableproducts.adapter.ProductsListAdapter
 import com.kakautrade.lanzarseapp.ui.availableproducts.viewmodel.ProductsListViewModel
 
-/**
- * A fragment representing a list of Items.
- */
 class AvailableProductsFragment : Fragment() {
 
-    private var columnCount = 1
-    val productsListViewModel: ProductsListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
+    private val productsListViewModel: ProductsListViewModel by viewModels()
+    private var _binding: FragmentAvailableProductsListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_available_products_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = LinearLayoutManager(context)
-                productsListViewModel.products.observe(viewLifecycleOwner) { productsList ->
-                    adapter = ProductsListAdapter(productsList)
-                }
-            }
-        }
-        return view
+    ): View {
+        _binding = FragmentAvailableProductsListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
+        binding.list.layoutManager = LinearLayoutManager(context)
 
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            AvailableProductsFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+        productsListViewModel.products.observe(viewLifecycleOwner) { productsList ->
+            binding.list.adapter = ProductsListAdapter(productsList)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
